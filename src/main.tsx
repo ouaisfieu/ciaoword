50335ce8bc5f02176a81bcff1d6a69872ee422a0
Collectif citoyen <ouaisfieux@gmail.com>
2026-01-05 16:02:36 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AppProvider } from './lib/AppContext.tsx'
-import { I18nProvider } from './lib/i18n.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <I18nProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </I18nProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>,
 )
